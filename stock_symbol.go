c71bb204ed3e48acd5e1038aedfa64da6f64c834
Tony Mackay <hello@tonymackay.net>
2020-09-29 12:59:23 +0100

    Add omitempty to StockSymbol

--- stock_symbol.go
+++ stock_symbol.go
@@ -9,13 +9,13 @@ package finnhub
 // to return a list of symbols for a given exchange.
 type StockSymbol struct {
 	// Symbol description (company or fund name etc)
-	Description string `json:"description"`
+	Description string `json:"description,omitempty"`
 	// Display symbol name.
-	DisplaySymbol string `json:"displaySymbol"`
+	DisplaySymbol string `json:"displaySymbol,omitempty"`
 	// Unique symbol used to identify this symbol used in /stock/candle endpoint.
-	Symbol string `json:"symbol"`
+	Symbol string `json:"symbol,omitempty"`
 	// Security type.
-	Type string `json:"type"`
+	Type string `json:"type,omitempty"`
 	// Price's currency. This might be different from the reporting currency of fundamental data.
-	Currency string `json:"currency"`
+	Currency string `json:"currency,omitempty"`
 }
